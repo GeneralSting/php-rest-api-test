931e7d56be5302fde72406e3afc615117241242d
GeneralSting <pavo.skocibusic@gmail.com>
2024-07-14 21:31:45 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+spl_autoload_register(function ($class) {
+  require __DIR__ . "/src/$class.php";
+});
+
+header("Content-type: application/json; charset=UTF-8");
+
+$parts = explode("/", $_SERVER["REQUEST_URI"]);
+
+if ($parts[1] != "products") {
+  http_response_code(404);
+  exit;
+}
+
+$id = $parts[2] ?? null;
+
+$database = new Database("localhost", "product_db", "root", "");
+
+$controller->processRequest($_SERVER["REQUEST_METHOD"], $id);
